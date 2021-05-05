f1515092427d0a9e36b5d7b8b319717cdafc0558
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-05-04 22:18:50 -0300

    Refactor: Content to Content Component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,49 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from './MovieCard';
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  Category: string;
+  GenreId: number;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);  
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.GenreId}`).then(response => {
+      setMovies(response.data);
+    });    
+  }, [props.GenreId]);  
+
+  console.log('Category', props.Category);
+
+
+  return (
+      <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.Category}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>    
+  )
 }
