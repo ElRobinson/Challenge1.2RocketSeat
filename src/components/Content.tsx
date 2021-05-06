787a2d2ae7c0fad1158a75600187bd5db9203fc9
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-05-05 22:12:37 -0300

    Fixed to use  context

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,49 +1,22 @@
-import { useEffect, useState } from 'react';
-import { MovieCard } from './MovieCard';
-import { api } from '../services/api';
+import { useMovies } from "../MoviesContext";
+import { MovieCard } from "./MovieCard";
 
-import '../styles/content.scss';
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
-interface ContentProps {
-  Category: string;
-  GenreId: number;
-}
-
-export function Content(props: ContentProps) {
-  const [movies, setMovies] = useState<MovieProps[]>([]);  
-  
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${props.GenreId}`).then(response => {
-      setMovies(response.data);
-    });    
-  }, [props.GenreId]);  
-
-  console.log('Category', props.Category);
-
+export function Content() {
+  const {selectedGenre, movies} = useMovies()
 
   return (
-      <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {props.Category}</span></span>
-      </header>
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
 
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
-      </main>
-    </div>    
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
   )
 }
